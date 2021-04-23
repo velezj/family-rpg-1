91ac8097ef8b5d08d273a5adbb926bd028dc6d53
Javier Velez <velejester@gmail.com>
2021-04-23 14:54:42 -0400

    Processing to allow SVG images with links (as exported from graphml using yEd)

---
+++ march-2021/notes/the-story-map.md
@@ -0,0 +1,6 @@
+---
+title: Story Map
+toc-title: Table of Contents
+---
+
+Here's a link to the current [story map](./story-map.svg), as an SVG exported from the [graphml](./story-map.graphml).
--- sitebuilder/hugo/greentide-horn/process-for-serving.fish
+++ sitebuilder/hugo/greentide-horn/process-for-serving.fish
@@ -8,6 +8,18 @@ cp -r $BASE content/
 
 for i in (find content/ -iname "*.md")
     echo "relinking '$i'"
-    awk -i inplace -e '{ print gensub(/\(([^/]\S+)\.md\)/,"(../\\\\1)","g",$0); }' "$i"
+    awk -i inplace -e '{ print gensub(/\(([^/]\S+)\)/,"(../\\\\1)","g",$0); }' "$i"
+    awk -i inplace -e '{ print gensub(/\(([^/]\S+)#(.*)\)/,"(../\\\\1#\\\\2)","g",$0); }' "$i"
 end
 
+for i in (find content/ -iname "*.svg")
+    echo "relinking '$i'"
+    awk -i inplace -e '{ print gensub(/href=\"([^/]\S+)\.md\"/,"href=\"../\\\\1\"","g",$0); }' "$i"
+    awk -i inplace -e '{ print gensub(/href=\"([^/]\S+)\.md#(\S*)\"/,"href=\"../\\\\1#\\\\2\"","g",$0); }' "$i"
+end
+
+for i in (find content/ -iname "*.html")
+    echo "relinking '$i'"
+    awk -i inplace -e '{ print gensub(/href=\"([^/]\S+)\"/,"href=\"../\\\\1\"","g",$0); }' "$i"
+    awk -i inplace -e '{ print gensub(/src=\"([^/]\S+)\"/,"src=\"../\\\\1\"","g",$0); }' "$i"
+end
