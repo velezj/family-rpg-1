3b9929c7ea95e51e735fbaea94a5cd3f7a5eab95
Javier Velez <velejester@gmail.com>
2021-10-08 12:04:55 -0400

    Changed to using Jinja2 templates for web page views

--- indexer-tantivy/search_server.py
+++ indexer-tantivy/search_server.py
@@ -9,7 +9,7 @@ import json
 from flask import Flask, Response
 import flask
 
-APP = Flask(__name__)
+APP = Flask(__name__, static_url_path='')
 
 INDEX_PATH = "session-index-001"
 
@@ -20,6 +20,9 @@ def _log():
 def _root():
     return "<p>root</p>"
 
+@APP.route('/static/<path:path>')
+def send_static(path):
+    return flask.send_from_directory('static', path)
 
 @APP.route("/absolute_file/<path:filepath>")
 def _send_absolute_file(filepath):
@@ -27,7 +30,7 @@ def _send_absolute_file(filepath):
         with open("/" + filepath) as f:
             return Response(f.read(), mimetype='text/plain')
     else:
-        return flask.send_file(filepath)
+        return flask.send_file("/" + filepath)
 
 def _buildup_html_li_for_search_result_item(data, context_size=5):
     filename = data['filename'][0]
@@ -64,8 +67,91 @@ def _buildup_html_li_for_search_result_item(data, context_size=5):
     fragment = "<li>" + fragment + "</li>"
     return fragment
 
+
+def _augment_data_items(data_items, context_size):
+    augmented = []
+    for item in data_items:
+        augmented.append(_augment_single_item(item, context_size))
+    return augmented
+
+def _line_number_from_id(line_id):
+    if line_id.strip() == "":
+        return None
+    if "." in line_id:
+        return int(line_id[:line_id.index(".")])
+    return int(line_id)
+
+def _augment_single_item(data, context_size):
+    filename = data['filename'][0].strip()
+    line_id = data['start_line_number'][0].strip()
+    line_number = _line_number_from_id(line_id)
+    target_index = context_size
+    if line_number is not None and line_number <= context_size:
+        target_index -= (context_size - line_number + 1)
+    before_lines = []
+    target_line = data['line'][0]
+    after_lines = []
+    if line_id != '':
+        res = subprocess.run(
+            ["python",
+             "file_line_context.py",
+             filename,
+             line_id,
+             str(context_size),
+             "true"],
+            capture_output=True,
+            text=True)
+        res.check_returncode()
+        lines = json.loads(res.stdout)
+        for i, line in enumerate(lines):
+            if i < target_index:
+                before_lines.append(line)
+            elif i > target_index:
+                after_lines.append(line)
+    return {
+        'item': data,
+        'filename': filename,
+        'line_number': line_number,
+        'before_lines': before_lines,
+        'after_lines': after_lines,
+        'target_line': target_line,
+        'context_size': context_size,
+        'target_index': target_index
+    }
+
+
+def _run_query(text_query, context_size=5):
+    res = subprocess.run(
+        ["tantivy",
+         "search",
+         "--index",
+         INDEX_PATH,
+         "--query",
+         text_query],
+        capture_output=True,
+        text=True)
+    if res.returncode != 0:
+        return (None, res.stderr)
+    data_items = []
+    for line in res.stdout.split("\n"):
+        if line.strip() != "":
+            data_items.append(json.loads(line))
+    data_items = _augment_data_items(data_items, context_size)
+    return (data_items, res.stderr)
+
 @APP.route("/search/<text_query>")
 def _search(text_query):
+    data_items, errstring = _run_query(text_query)
+    if data_items is None:
+        return flask.render_template("search_error_template.jinja2",
+                                     query=text_query,
+                                     errstring=errstring)
+    return flask.render_template("search_results_template.jinja2",
+                                 query=text_query,
+                                 data_items=data_items)
+
+@APP.route("/search/old/<text_query>")
+def _search_old(text_query):
     res = subprocess.run(
         ["tantivy",
          "search",
